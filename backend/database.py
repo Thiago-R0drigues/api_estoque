4380cfcf0ced59820d344b21e8e00c6e571c4562
Thiago Oliveira <trodrigues361@gmail.com>
2026-07-23 15:14:34 -0400

    Migra banco de SQLite para PostgreSQL e organiza backend em camadas (main, crud, schemas, models, database)

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,20 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = 'sqlite:///estoque.db'
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread':False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("A variável de ambiente DATABASE_URL não foi definida!")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
 
 def get_db():
     db = SessionLocal()
